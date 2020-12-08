1bb5b2ffcf0689fcf8ba24e665629bf1fd19d1d1
chaitanya <chaitu.innamuri@gmail.com>
2020-12-08 16:22:53 +0530

    added all files

---
+++ src/main/java/practice/loop/LoopApplication.java
@@ -0,0 +1,13 @@
+package practice.loop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopApplication.class, args);
+	}
+
+}
