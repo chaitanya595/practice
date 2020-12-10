548b42d56fb347fd0656d9d83ee88204de3b7241
chaitanya <chaitu.innamuri@gmail.com>
2020-12-10 12:37:53 +0530

    code changes

--- src/main/java/practice/loop/LoopApplication.java
+++ src/main/java/practice/loop/LoopApplication.java
@@ -2,12 +2,19 @@ package practice.loop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot:web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoopApplication {
+public class LoopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoopApplication.class, args);
+	}
+
+	@override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(LoopApplication.class);
 	}
 
 }
